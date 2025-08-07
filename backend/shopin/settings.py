4b51c06a9c9a6b67475a30e503552a3ba4716477
ramo1990 <abudul917@gmail.com>
2025-08-07 20:43:33 +0200

    Configuration de s3 bucket

--- backend/shopin/settings.py
+++ backend/shopin/settings.py
@@ -216,3 +216,6 @@ AWS_S3_VERITY = True
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
