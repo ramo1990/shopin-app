1f0aea423f4a5d1acc0b7c230e28e3553f6e0a5d
ramo1990 <abudul917@gmail.com>
2025-08-07 20:30:00 +0200

    Configuration de s3 bucket

--- backend/shopin/settings.py
+++ backend/shopin/settings.py
@@ -201,3 +201,18 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 DEFAULT_FROM_EMAIL = 'no-reply@shopin.com'
 CONTACT_RECEIVER_EMAIL = 'admin@tonsite.com'  # où tu veux recevoir les messages
+
+
+# AWS S3 BUCKET
+# Variables d'environnement (ne jamais les exposer directement)
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'nextjs-django'
+AWS_S3_SIGNATURE_NAME = 's3v4'
+AWS_S3_REGION_NAME = 'eu-north-1'
+AWS_S3_FILE_OVERWRITE = False
+# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_VERITY = True
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
