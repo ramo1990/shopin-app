3bf85ead5a41b53787c3f0cdda3e74cd69dee386
ramo1990 <abudul917@gmail.com>
2025-08-08 20:31:48 +0200

    commit avant creation de branch

--- backend/shopin/settings.py
+++ backend/shopin/settings.py
@@ -211,7 +211,6 @@ AWS_STORAGE_BUCKET_NAME = 'nextjs-django'
 AWS_S3_SIGNATURE_NAME = 's3v4'
 AWS_S3_REGION_NAME = 'eu-north-1'
 AWS_S3_FILE_OVERWRITE = False
-# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_VERITY = True
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
